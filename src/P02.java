757945abf5f2e88bf91356b08df16f578ed98bff
Cpik <cepik.kbrnr@gmail.com>
2024-05-03 16:46:10 +0300

    second version

---
+++ src/P02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P02 {
+    public static void main(String[] args) {
+
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Adinizi giriniz");
+        String isim=scanner.nextLine();
+
+    }
+}
